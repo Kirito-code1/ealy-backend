eb43fb0cacc6a4d1a719232817bd0e34644efd3b
Kirito-code1 <yigitalievshahzod821@gmail.com>
2026-01-15 19:11:58 +0500

    add-another

--- main.py
+++ main.py
@@ -11,10 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "https://eatly-website-frontend.up.railway.app"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
